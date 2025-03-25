efb0bd2875150a310ad3a042257f17a845e1a27c
ChaitanyaS <chaitanya.s@pei.group>
2025-03-26 00:52:56 +0530

    Added treesitter queries for sscript

---
+++ queries/sscript/highlights.scm
@@ -0,0 +1,11 @@
+(slug) @type
+; Highlight character names
+(character_name) @constant
+
+; Highlight dialogue
+(dialogue_line) @string
+
+; Highlight actions
+(action) @keyword
+
+(transition) @type
