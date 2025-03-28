d26a0f0530c9a6d146443224d2ec1b0fff7769eb
ChaitanyaS <chaitanya.s@pei.group>
2025-03-28 14:25:45 +0530

    Added completions to ; key

--- lua/custom/plugins/completions.lua
+++ lua/custom/plugins/completions.lua
@@ -1,3 +1,13 @@
 return {
   'github/copilot.vim',
+
+  config = function()
+    vim.keymap.set('i', ';', 'copilot#Accept("\\<CR>")', {
+      expr = true,
+      replace_keycodes = false,
+    })
+
+    vim.keymap.set('i', '<Tab>', ';')
+    vim.g.copilot_no_tab_map = true
+  end,
 }
