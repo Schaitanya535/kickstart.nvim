3db0b1915c62e78f752824e6d9d5d89a6290341a
ChaitanyaS <chaitanya.s@pei.group>
2025-03-28 14:19:45 +0530

    Added harpoon

--- lua/custom/plugins/completions.lua
+++ lua/custom/plugins/completions.lua
@@ -1,11 +1,3 @@
 return {
   'github/copilot.vim',
-
-  config = function()
-    vim.keymap.set('i', ';', 'copilot#Accept("\\<CR>")', {
-      expr = true,
-      replace_keycodes = false,
-    })
-    vim.g.copilot_no_tab_map = true
-  end,
 }
---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,50 @@
+return {
+
+  'ThePrimeagen/harpoon',
+
+  config = function()
+    require('harpoon').setup {
+      global_settings = {
+        -- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
+        save_on_toggle = false,
+
+        -- saves the harpoon file upon every change. disabling is unrecommended.
+        save_on_change = true,
+
+        -- sets harpoon to run the command immediately as it's passed to the terminal when calling `sendCommand`.
+        enter_on_sendcmd = false,
+
+        -- closes any tmux windows harpoon that harpoon creates when you close Neovim.
+        tmux_autoclose_windows = false,
+
+        -- filetypes that you want to prevent from adding to the harpoon list menu.
+        excluded_filetypes = { 'harpoon' },
+
+        -- set marks specific to each git branch inside git repository
+        mark_branch = false,
+
+        -- enable tabline with harpoon marks
+        tabline = false,
+        tabline_prefix = '   ',
+        tabline_suffix = '   ',
+      },
+    }
+
+    require('telescope').load_extension 'harpoon'
+
+    vim.keymap.set('n', '<leader>ha', require('harpoon.mark').add_file, { desc = 'Add file to Harpoon' })
+    vim.keymap.set('n', '<leader>hm', require('harpoon.ui').toggle_quick_menu, { desc = 'Toggle Harpoon menu' })
+    vim.keymap.set('n', '<leader>1', function()
+      require('harpoon.ui').nav_file(1)
+    end, { desc = 'Go to file 1' })
+    vim.keymap.set('n', '<leader>2', function()
+      require('harpoon.ui').nav_file(2)
+    end, { desc = 'Go to file 2' })
+    vim.keymap.set('n', '<leader>3', function()
+      require('harpoon.ui').nav_file(3)
+    end, { desc = 'Go to file 3' })
+    vim.keymap.set('n', '<leader>4', function()
+      require('harpoon.ui').nav_file(4)
+    end, { desc = 'Go to file 4' })
+  end,
+}
