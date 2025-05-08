27994ee27a77b692cdeb1bfd1d82d801ecfa649c
ChaitanyaS <chaitanya.s@pei.group>
2025-05-08 23:19:26 +0530

    Added mcp server support to avante

--- lua/custom/plugins/completions.lua
+++ lua/custom/plugins/completions.lua
@@ -33,6 +33,16 @@ return {
           insert = '<C-e>',
         },
       },
+      system_prompt = function()
+        local hub = require('mcphub').get_hub_instance()
+        return hub:get_active_servers_prompt()
+      end,
+      -- The custom_tools type supports both a list and a function that returns a list. Using a function here prevents requiring mcphub before it's loaded
+      custom_tools = function()
+        return {
+          require('mcphub.extensions.avante').mcp_tool(),
+        }
+      end,
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = 'make',
